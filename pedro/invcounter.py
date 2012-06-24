3b8c2a91d7a17e971b35eed70e9777b97914b77b
Pedro Romano <pedro.romano@digitaluna.net>
2012-06-24 13:28:02 +0100

    Fully functional version of inversions counter. Provides correct answer for 1st week problem.

--- pedro/invcounter.py
+++ pedro/invcounter.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 
 
-def merge(left, right):
+def merge_and_count_inv((left_inversions, left), (right_inversions, right)):
     """
-    Generate merged ordered values from two ordered sequences.
+    Merge values from two ordered sequences and count the split inversions.
     """
     # check trivial cases where at least one of the sequences is empty.
     if not left:
-        return right
+        return (right_inversions, right)
     if not right:
-        return left
+        return (left_inversions, left)
 
-    # initialise merged result list and indexes for left and right sequences
+    # initialise merged result list, indexes and lengths for left and right
+    # sequences and inversions counter
     merged = []
     left_index = 0
     right_index = 0
     left_length = len(left)
     right_length = len(right)
+    inversions = left_inversions + right_inversions
 
     # merge until one of the sub-sequences is empty
     while left_index < left_length and right_index < right_length:
@@ -26,6 +28,7 @@ def merge(left, right):
         else:
             merged.append(right[right_index])
             right_index += 1
+            inversions += left_length - left_index
 
     # append the remaining of the non-empty sub-sequence
     if left_index < left_length:
@@ -33,15 +36,21 @@ def merge(left, right):
     else:
         merged.extend(right[right_index:])
 
-    return merged
+    return (inversions, merged)
 
 
-def merge_sort(numbers):
+def sort_and_count_inv(numbers):
     """
     Implement the (recursive) merge sort algorithm.
     """
     if len(numbers) <= 1:
-        return numbers
+        return (0, numbers)
     else:
         half = len(numbers) // 2
-        return merge(merge_sort(numbers[:half]), merge_sort(numbers[half:]))
+        return merge_and_count_inv(sort_and_count_inv(numbers[:half]),
+                                   sort_and_count_inv(numbers[half:]))
+
+if __name__ == '__main__':
+    with open('IntegerArray.txt') as array_file:
+        numbers = [int(number.strip()) for number in array_file.readlines()]
+    print(sort_and_count_inv(numbers)[0])
--- pedro/invcounter_tests.py
+++ pedro/invcounter_tests.py
@@ -3,34 +3,49 @@
 import random
 import unittest
 
-from invcounter import merge, merge_sort
+from invcounter import merge_and_count_inv, sort_and_count_inv
 
 
 class TestMergeSort(unittest.TestCase):
 
-    def test_merge_trivial_1(self):
-        self.assertEqual(merge([], []), [])
+    def test_merge_and_count_inv_1(self):
+        self.assertEqual(merge_and_count_inv((0, []), (0, [])), (0, []))
 
-    def test_merge_trivial_1(self):
-        self.assertEqual(merge([], [1]), [1])
+    def test_merge_and_count_inv_2(self):
+        self.assertEqual(merge_and_count_inv((0, []), (0, [1])), (0, [1]))
 
-    def test_merge_trivial_1(self):
-        self.assertEqual(merge([1], []), [1])
+    def test_merge_and_count_inv_3(self):
+        self.assertEqual(merge_and_count_inv((0, [1]), (0, [])), (0, [1]))
 
-    def test_merge_trivial_1(self):
-        self.assertEqual(merge([2], [1]), [1, 2])
+    def test_merge_and_count_inv_4(self):
+        self.assertEqual(merge_and_count_inv((0, [1]), (0, [2])), (0, [1, 2]))
 
-    def test_merge_balanced(self):
-        self.assertEqual(merge([1, 3, 5], [2, 4, 6]), [1, 2, 3, 4, 5, 6])
+    def test_merge_and_count_inv_5(self):
+        self.assertEqual(merge_and_count_inv((0, [2]), (0, [1])), (1, [1, 2]))
 
-    def test_merge_unbalanced(self):
-        self.assertEqual(merge([1, 3, 5], [4]), [1, 3, 4, 5])
+    def test_merge_and_count_inv_6(self):
+        self.assertEqual(merge_and_count_inv((0, [1, 2]), (0, [3, 4])),
+                         (0, [1, 2, 3, 4]))
 
-    def test_merge_sort(self):
-        sorted_numbers = list(range(1000000))
-        shuffled_numbers = sorted_numbers[:]
-        random.shuffle(shuffled_numbers)
-        self.assertEqual(merge_sort(shuffled_numbers), sorted_numbers)
+    def test_merge_and_count_inv_6(self):
+        self.assertEqual(merge_and_count_inv((0, [3, 4]), (0, [1, 2])),
+                         (4, [1, 2, 3, 4]))
+
+    def test_sort_and_count_inv_1(self):
+        self.assertEqual(sort_and_count_inv([]), (0, []))
+
+    def test_sort_and_count_inv_2(self):
+        self.assertEqual(sort_and_count_inv([1]), (0, [1]))
+
+    def test_sort_and_count_inv_3(self):
+        self.assertEqual(sort_and_count_inv([1, 2]), (0, [1, 2]))
+
+    def test_sort_and_count_inv_4(self):
+        self.assertEqual(sort_and_count_inv([2, 1]), (1, [1, 2]))
+
+    def test_sort_and_count_inv_5(self):
+        self.assertEqual(sort_and_count_inv([6, 5, 4, 3, 2, 1]),
+                         (15, [1, 2, 3, 4, 5, 6]))
 
 if __name__ == '__main__':
     unittest.main()
