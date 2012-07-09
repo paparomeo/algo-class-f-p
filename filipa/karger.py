15d94b1cf9fe132826cae6acef7f3cd1443247c7
Filipa Andrade <filipa.andrade@gmail.com>
2012-07-09 02:03:27 +0200

    Finishes implementation of karger and runs it several times to check the mincut

--- filipa/karger.py
+++ filipa/karger.py
@@ -16,9 +16,8 @@ def remove_self_loops(graph, node_label):
     """
     Remove edges from any node to itself
     """
-    connecting_nodes = graph[node_label]
     graph[node_label] = filter(lambda a: a != node_label, 
-                               connecting_nodes)
+                               graph[node_label])
 
 
 def karger(graph):
@@ -27,21 +26,43 @@ def karger(graph):
     """
     # Stop if number of nodes is 2
     if len(graph) == 2:
-        return graph
+        for node in graph:
+            print len(graph[node])
+            return
 
     # Chose a random edge
     node_label = random.choice(graph.keys())
 
     # Contract the graph
-
-    # Remove self loops
-    remove_self_loops(graph, node_label)
-
+    node_label2 = graph[node_label][0]
+    # Removes the node that will eat this one 
+    # from connections. All occurences
+    graph[node_label] = filter(lambda a: a != node_label2,
+                               graph[node_label])
+    # Removes the node to disapear from the second
+    # All occurences
+    graph[node_label2] = filter(lambda a: a != node_label,
+                                graph[node_label2])
+    # Adds the connections of the first to the second
+    graph[node_label2].extend(graph[node_label])
+    
+    # Changes first node into second node in all other nodes
+    for node in graph:
+        if node_label in graph[node]:
+            graph[node] = [x if x != node_label else node_label2 for x in graph[node]]
+    
+    # Removes the first node
+    del graph[node_label]
+                            
     # Calls again the algorithm
-    #karger(graph)
+    karger(graph)
 
 
 if __name__ == '__main__':
     # Exercise 3
-    graph = make_graph()
-    karger(graph)
+    i = 0
+    while i < 200 ** 2:
+        graph = make_graph()
+        n = len(graph)
+        karger(graph)
+        i += 1
