3c327a627e5e683b9e4e4d47369f3ac7f8c319df
Filipa Andrade <filipa.andrade@gmail.com>
2012-07-28 17:22:29 +0200

    Test file and finished algorithm

--- filipa/scc.py
+++ filipa/scc.py
@@ -1,102 +1,65 @@
 # encoding:utf-8
 import collections
+import sys
 
-
-FINAL_NODE = 875714
+FINAL_NODE = 875715
 t = 0
 s = None
-graph = None
-reversed_graph = None
-finishing_times = []
-leaders = {}
-
+graph = collections.defaultdict(lambda: [])
+reversed_graph = collections.defaultdict(lambda: [])
+finish_order = []
+sizes = []
+reverse_explored = set()
+explored = set()
+finish_2 = []
 
 def make_graphs():
     global graph, reversed_graph
-    # defaultdict allows the value of the key to be something
-    graph = collections.defaultdict(lambda: {'explored': False, 'arcs': []})
-    reversed_graph = collections.defaultdict(lambda: {'explored': False, 'arcs': []})
     with open('SCC.txt') as f:
         for i, line in enumerate(f.readlines()):
             line = line.strip().split(' ')
-            # Adds nodes to graph
-            graph[line[0]]['arcs'].append(line[1])
-            reversed_graph[line[1]]['arcs'].append(line[0])
+            # Adds nodes to graphs
+            head = int(line[0])
+            tail = int(line[1])
+            graph[head].append(tail)
+            reversed_graph[tail].append(head)
 
 
-def dfs_loop_reversed():
-    print 'dfs loop reversed'
-    global reversed_graph
-
-    def dfs(initial):
-        global t, s, finishing_times, reversed_graph
-        head = reversed_graph[initial]
-        # Mark head as explored
-        head['explored'] = True
-        print 'reversed graph head: ', head
-        for tail in head['arcs']:
-            if not reversed_graph[tail]['explored']:
-                dfs(tail)
-            else:
-                t += 1
-                finishing_times.append((t, tail))
-                print 'appending to finishing times'
-
-    i = FINAL_NODE
-    while i > 0:
-        node = str(i)
-        print 'analising node ', node, reversed_graph[node]
-        if not reversed_graph[node]['explored']:
-            dfs(node)
-        i -= 1
+def dfs(graph, initial, is_reversed=False):
+    explored.add(initial)
+    for tail in graph[initial]:
+        if tail not in explored:
+            dfs(graph, tail, is_reversed)
+    if is_reversed:
+        #print 'appending to finishing times ', initial
+        finish_order.append(initial)
+    else:
+        finish_2.append(initial)
 
 
-def dfs_loop():
-    print 'dfs loop'
-    global s, graph
-
-    def dfs(initial):
-        global s, graph
-        head = graph[initial]
-        # Mark head as explored
-        head['explored'] = True
-        for tail in head['arcs']:
-            if not graph[tail]['explored']:
-                dfs(tail)
-            else:
-                leaders.append((s, tail))
-                print 'appending to leaders'
-
-    order = order_finishing_times()
+def dfs_loop(graph, order, is_reversed=False):
     for it in order:
-        node = it[1]
-        if not graph[node]['explored']:
-            s = node
-            dfs(node)
-
-
-def order_finishing_times():
-    print finishing_times[:10]
-    sorted_finishing_times = sorted(finishing_times)
-    return sorted_finishing_times.reverse()
-
+        if it not in explored:
+            before = len(finish_2)
+            dfs(graph, it, is_reversed)
+            if not is_reversed:
+                sizes.append(len(finish_2) - before)
 
 def scc():
-    """
-    Implements the strongly connected components algorithm
-    """
-    dfs_loop_reversed()
-    dfs_loop()
-
-
-def find_sccs(graph):
-    leaders = [node['leader'] for node in graph]
-    collections.Counter(leaders)
+    dfs_loop(reversed_graph, reversed(xrange(1, FINAL_NODE)), True)
+    global explored, finish_2
+    explored = set()
+    finish_2 = []
+    dfs_loop(graph, reversed(finish_order))
 
 
 if __name__ == '__main__':
     # Exercise 4
+    sys.setrecursionlimit(27000)
     make_graphs()
     print len(graph), len(reversed_graph)
     scc()
-    find_sccs(graph)
+    print 'number of sccs: ', len(sizes)
+    sccs = sorted(sizes)
+    sccs.reverse()
+    print sccs[:10]
